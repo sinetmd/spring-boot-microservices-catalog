83f3a76211fa911cbad8b473c54e6e26e7627708
sinetmd <dobremrn12@gmail.com>
2024-04-15 15:24:40 +0300

    catalog service setup

---
+++ catalog-service/src/test/java/com/mrn/catalogservice/TestCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mrn.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestCatalogServiceApplication.class)
+                .run(args);
+    }
+}
